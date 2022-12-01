fc9a6070aa7f7106f24a5f2a593c853fdd50cc98
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-12-01 22:50:21 +0100

    Slimmed

--- 2022/1/1_december.py
+++ 2022/1/1_december.py
@@ -1,23 +1,14 @@
 file = open("input.txt","r")
 data = file.read().split("\n\n")
 
-def first_star(Data):
-    result = 0 
-    for i,elf in enumerate(Data):
-        if i == (len(Data)-1): elf = elf[:len(elf)-1] 
-        cal= list(map(int,elf.split("\n")))
-        result = max(sum(cal),result)
-    return result 
-
-def second_star(Data):
+def snack_provider(Data):
     elfs = [] 
     for i,elf in enumerate(Data):
         if i == (len(Data)-1): elf = elf[:len(elf)-1] 
         cal= list(map(int,elf.split("\n")))
         elfs.append(sum(cal)) 
     elfs.sort()
-    return sum(elfs[-3:]) 
+    return sum(elfs[-1:]),sum(elfs[-3:]) 
 
-
-#print("First star: ",first_star(data))
-print("Second star: ",second_star(data))
+firstStar,secondStar = snack_provider(data)
+print(f"First star result:{firstStar}\nSecond star result: {secondStar}")
