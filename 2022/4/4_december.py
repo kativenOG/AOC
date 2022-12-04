fd3e102505364ccd29b0e8eac348aae4dd20355e
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-12-04 14:43:49 +0100

    Fixed range mistake

--- 2022/4/4_december.py
+++ 2022/4/4_december.py
@@ -5,7 +5,8 @@ def first_star(Data):
     for line in Data:
         ranges = line.split(",")
         first,second= list(map(int,ranges[0].split("-"))) , list(map(int,ranges[1].split("-")))
-        if (second[0] >= first[0] and first[1] <= second[1]) or (first[0] >= second[0] and second[1] <= first[1]):
+        if ((second[0] <= first[0]) and (first[1] <= second[1])) or ((first[0] <= second[0]) and (second[1] <= first[1])):
+            print("Target: ",first,second)
             result+=1
     return result 
 
