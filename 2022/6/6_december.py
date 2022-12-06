166819c37993f886480925301625829ab02cdd94
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-12-06 22:24:41 +0100

    Sesto giorno easyyyy (ci ho messo solo 40 minuti D: )

---
+++ 2022/6/6_december.py
@@ -0,0 +1,29 @@
+data = open("./input.txt","r").read()
+
+# Checcka tutti i numeri succesivi nella finestra partendo dal primo, esce subito alla prima uguaglianza essendo all() una serie di and
+# Se avessi le palle lo riscriveresti con ricorsione
+def checker(slice): 
+    # print("Slice: ",slice)
+    selected = slice[0]
+    if all(selected != val for val in slice[1:]):
+        return True 
+    else: return False
+
+def first_star(Data):
+    for i in range(len(Data)):
+        print("\n")
+        if i>2:
+            if all( checker(data[(i-pos) :i+1]) for pos in [3,2,1]):
+                return i+1
+    return None
+
+def second_star(Data):
+    for i in range(len(Data)):
+        print("\n")
+        if i>12:
+            if all( checker(data[(i-pos) :i+1]) for pos in [13,12,11,10,9,8,7,6,5,4,3,2,1]):
+                return i+1
+    return None
+
+
+print(f"First Star: {first_star(data)}\nSecond Star: {second_star(data)}")
