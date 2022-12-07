7808355ec9f544213a477778f0083c3b2888e45a
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-12-07 16:23:44 +0100

    Day 5 first star only because im dumb

---
+++ 2022/5/5_december.py
@@ -0,0 +1,48 @@
+file = open("./input.txt").read().splitlines()
+
+# Getting the initial configuration
+n  = max(list(map(int,file[8].split()))) # Number of Colums 
+cargo_ship = [[] for _ in range(n)]
+for line in file[:10]: # Carico i Carichi 
+    for i,char in enumerate(line):
+        if char.isalpha(): cargo_ship[round(i/4)].append(char)
+# Inverto le stack per avere come ultimo elemento quello che è da poppare 
+for i in range(n): 
+    cargo_ship[i] = cargo_ship[i][::-1]
+
+# Lista dei Comandi ! 
+commands = file[10:] 
+
+def first_star(commandss,cargo):
+    # Re-arranging Crates: CrateMover 9000
+    for command in commandss:
+        command = command.split()
+        for _ in range(int(command[1])):
+            gru = cargo[int(command[3])-1].pop()
+            cargo[int(command[5])-1].append(gru)
+
+    # Calculating the Result 
+    result = []
+    for i in range(9):
+        result.append(cargo[i][len(cargo[i])-1])
+    return result
+
+def second_star(commandss,cargo):
+    # Re-arranging Crates: CrateMover 9001
+    for command in commandss:
+        command = command.split()
+        gru = []
+        for _ in range(int(command[1])):
+            appo = cargo[int(command[3])-1].pop()
+            gru.append(appo) 
+        for appo in gru:
+            cargo[int(command[5])-1].append(appo)
+
+    # Calculating the Result 
+    result = []
+    for i in range(9):
+        result.append(cargo[i][len(cargo[i])-1])
+    return result
+
+# print(f"First Star: {first_star(commands,cargo_ship)}\n")
+print(f"Second Star: {second_star(commands,cargo_ship)}")
