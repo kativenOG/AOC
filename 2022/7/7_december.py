d400fa35d15351258cbbb7e9ebb094332303380f
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-12-07 13:32:49 +0100

    Seven day finished

--- 2022/7/7_december.py
+++ 2022/7/7_december.py
@@ -1,4 +1,5 @@
 MAX_SIZE = 100000
+UPDATE_SIZE= 30000000  
 data = open("./input.txt","r").read().splitlines()
 
 class Node():
@@ -13,41 +14,50 @@ class Node():
         if self.parent!= None: self.parent.update(size)
 
     def insert(self,name) :
-        self.childs.append(Node(name,self))
-        return self.childs[len(self.childs)-1]
-    
-    def backward(self):
-        return self.parent
+        child = Node(name,self)
+        self.childs.append(child)
+        return child
     
     def sizes(self)-> int:
-        counter = 0 if self.size > MAX_SIZE else 1 
-        print(f"\nName: {self.name} Size: {self.size}")
-        if len(self.childs)>0:
-            for val in self.childs:
-                counter += val.sizes() 
-        return counter
+        total = 0 if self.size > MAX_SIZE else self.size 
+        # print(f"\nName: {self.name} Size: {self.size}")
+        for val in self.childs:
+            total+= val.sizes() 
+        return total 
+    
+    def update_dir(self,space_to_free)->int:
+        target = 70000000
+        if self.size> space_to_free:
+            target = self.size
+        for val in self.childs:
+                target = min(target,val.update_dir(space_to_free)) 
+        return target
 
-    # def print(self):
-    #     print(f"Name: {self.name} Size: {self.size} Father: {self.parent}")
+    def print(self):
+        print(f"\nName: {self.name} Size: {self.size} Father: {self.parent}")
 
 # Ignora le linee dir <name> (crei solo con cd)  e ls !! 
-def first_star(Data)-> int:
+def results(Data):
     root= Node("root",None)
     current_pos = root
     for line in Data:
+
         if line[0] == "$": # Commands: (non serve controllare ls)
             command = line.split()[1:]
             if command[0] == "cd":
                 if command[1] == "..":
-                    current_pos = current_pos.backward()
+                    current_pos = current_pos.parent
                 else:
                     current_pos = current_pos.insert(command[1])
+
         else:
             command = line.split()
             if command[0].isnumeric():
                 current_pos.update(int(command[0]))
          
-    targets = root.sizes() 
-    return targets
+    unused = 70000000 - root.size 
+    missing_space = 30000000 - unused
+    return root.sizes(), root.update_dir(missing_space)
 
-print(f"First Star: {first_star(data)}\n")
+targets,update_size =  results(data)
+print(f"\nFirst Star: {targets}\nSecond Star: {update_size}")
