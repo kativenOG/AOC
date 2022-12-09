e26100255bff038487c3eca34739593f87d88f94
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-12-09 15:35:37 +0100

    First Star of 9

---
+++ 2022/9/9_december.py
@@ -0,0 +1,39 @@
+commands = open("./input.txt","r").read().splitlines()
+
+def first_star(Commands):
+    t,h = [0,0],[0,0] # s = 0,0
+    t_visited_pos = set()
+    t_visited_pos.add(tuple(t))
+    # t_visited_pos = []
+    # t_visited_pos.append(tuple(t))
+    for command in Commands:
+        # Parsing the command
+        direction , steps = command.split()[0],int(command.split()[1])
+        sum = True if (direction=="U" or direction=="R") else False
+        axis = 1 if (direction=="U" or direction=="D") else 0 
+        for _ in range(steps):
+            # Moving the head
+            if sum: h[axis] += 1 
+            else: h[axis] -=1
+
+            # Moving the tail: im not moving diagonally when needed 
+            x_diff, y_diff = h[0]-t[0],h[1]-t[1]
+            if (x_diff> 1 or x_diff<-1) and (y_diff == 1 or y_diff==-1):
+                t[0] += int(x_diff/abs(x_diff))
+                t[1] += y_diff
+            elif (y_diff> 1 or y_diff<-1) and (x_diff == 1 or x_diff==-1):
+                t[0] += x_diff 
+                t[1] += int(y_diff/abs(y_diff))
+            else:
+                if (x_diff>1): t[0]+=1 # X coordinate change
+                elif (x_diff<-1): t[0]-=1 
+                if (y_diff>1): t[1]+=1 # Y coordinate change
+                elif (y_diff<-1): t[1]-=1 
+
+            # Updating tail previous positions array:
+            t_visited_pos.add(tuple(t))
+            # print(f"Tail: {tuple(t)}, Head: {tuple(h)}, DistanceX {h[0]-t[0]}, Distance Y: {h[1]-t[1]}")
+
+    return len(t_visited_pos)
+
+print(f"First Star: {first_star(commands)}\n")
