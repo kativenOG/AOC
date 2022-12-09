4a2d4ea67fc8735d94593797d2b2bad5e5f20f01
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-12-09 16:24:34 +0100

    day 9 second star

--- 2022/9/9_december.py
+++ 2022/9/9_december.py
@@ -1,3 +1,4 @@
+import copy 
 commands = open("./input.txt","r").read().splitlines()
 
 def first_star(Commands):
@@ -36,4 +37,32 @@ def first_star(Commands):
 
     return len(t_visited_pos)
 
+def second_star(Commands):
+    rope = [[0,0] for _ in range(10)]
+    t_visited_pos = set()
+
+    for command in Commands:
+        direction , steps = command.split()[0],int(command.split()[1])
+        sum = True if (direction=="U" or direction=="R") else False
+        axis = 1 if (direction=="U" or direction=="D") else 0 
+        for _ in range(steps):
+            if sum: rope[0][axis] += 1 
+            else: rope[0][axis] -=1
+            for knot in range(1,10):
+                x_diff, y_diff = rope[knot-1][0]-rope[knot][0],rope[knot-1][1]-rope[knot][1]
+                if (x_diff> 1 or x_diff<-1) and (y_diff == 1 or y_diff==-1):
+                    rope[knot][0] += int(x_diff/abs(x_diff))
+                    rope[knot][1] += y_diff
+                elif (y_diff> 1 or y_diff<-1) and (x_diff == 1 or x_diff==-1):
+                    rope[knot][0] += x_diff 
+                    rope[knot][1] += int(y_diff/abs(y_diff))
+                else:
+                    if (x_diff>1): rope[knot][0]+=1 # X coordinate change
+                    elif (x_diff<-1): rope[knot][0]-=1 
+                    if (y_diff>1): rope[knot][1]+=1 # Y coordinate change
+                    elif (y_diff<-1): rope[knot][1]-=1
+                t_visited_pos.add(tuple(rope[9]))
+    return len(t_visited_pos)
+new_rope = copy.deepcopy(commands)
 print(f"First Star: {first_star(commands)}\n")
+print(f"Second Star: {second_star(new_rope)}\n")
